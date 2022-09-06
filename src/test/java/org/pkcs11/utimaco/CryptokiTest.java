9760f0a4262e349da445d5bb9b6ff06b9201df1c
Jose Miranda <jose.miranda@devisefutures.com>
2022-09-06 15:47:20 +0100

    202209061546 Additional EdDSA tests

--- src/test/java/org/pkcs11/utimaco/CryptokiTest.java
+++ src/test/java/org/pkcs11/utimaco/CryptokiTest.java
@@ -30,6 +30,9 @@ import org.pkcs11.jacknji11.CKM;
 import org.pkcs11.jacknji11.CKR;
 import org.pkcs11.jacknji11.CKRException;
 import org.pkcs11.jacknji11.CK_SESSION_INFO;
+import org.pkcs11.jacknji11.CK_SLOT_INFO;
+import org.pkcs11.jacknji11.CK_TOKEN_INFO;
+import org.pkcs11.jacknji11.Hex;
 import org.pkcs11.jacknji11.LongRef;
 
 import junit.framework.TestCase;
@@ -81,46 +84,15 @@ public class CryptokiTest extends TestCase {
                 null, null);
         CE.LoginUser(session, USER_PIN);
 
-        // Attributes from PKCS #11 Cryptographic Token Interface Current Mechanisms
-        // Specification Version 2.40 section 2.3.3 - ECDSA public key objects
-        /*
-         * DER-encoding of an ANSI X9.62 Parameters, also known as
-         * "EC domain parameters".
-         */
-        // We use a Ed25519 key, the oid 1.3.101.112 has DER encoding in Hex 06032b6570
-        // In Utimaco, EC_PARAMS needs to have the value "edwards25519"
-
-        CKA[] pubTempl = new CKA[] {
-                new CKA(CKA.EC_PARAMS, "edwards25519"),
-                new CKA(CKA.WRAP, false),
-                new CKA(CKA.ENCRYPT, false),
-                new CKA(CKA.VERIFY, true),
-                new CKA(CKA.VERIFY_RECOVER, false),
-                new CKA(CKA.TOKEN, true),
-                new CKA(CKA.LABEL, "labelec-public"),
-                new CKA(CKA.ID, "labelec"),
-        };
-        CKA[] privTempl = new CKA[] {
-                new CKA(CKA.TOKEN, true),
-                new CKA(CKA.PRIVATE, true),
-                new CKA(CKA.SENSITIVE, true),
-                new CKA(CKA.SIGN, true),
-                new CKA(CKA.SIGN_RECOVER, false),
-                new CKA(CKA.DECRYPT, false),
-                new CKA(CKA.UNWRAP, false),
-                new CKA(CKA.EXTRACTABLE, false),
-                new CKA(CKA.LABEL, "labelec-private"),
-                new CKA(CKA.ID, "labelec"),
-        };
         LongRef pubKey = new LongRef();
         LongRef privKey = new LongRef();
-        CE.GenerateKeyPair(session, new CKM(CKM.ECDSA_KEY_PAIR_GEN), pubTempl, privTempl, pubKey, privKey);
+        generateKeyPairEd25519(session, pubKey, privKey);
 
         System.out.println("testSignVerifyEd25519: edwards25519 keypair generated. PublicKey handle: " + pubKey.value()
                 + ", PrivKey handle: " + privKey.value());
 
         // Direct sign, PKCS#11 "2.3.6 ECDSA without hashing"
-        byte[] data = new byte[32]; // SHA256 hash is 32 bytes
+        byte[] data = "MEssage to be signed!!".getBytes(); // SHA256 hash is 32 bytes
         CE.SignInit(session, new CKM(CKM.ECDSA), privKey.value());
         byte[] sig1 = CE.Sign(session, data);
         assertEquals(64, sig1.length);
@@ -132,6 +104,14 @@ public class CryptokiTest extends TestCase {
         CE.Verify(session, data, sig1);
 
         System.out.println("testSignVerifyEd25519: Signature verified");
+
+        CE.SignInit(session, new CKM(CKM.ECDSA), privKey.value());
+        byte[] sig3 = CE.Sign(session, data);
+
+        System.out.println(
+                "testSignVerifyEd25519: Signatures are the same: sig1 = " + Hex.b2s(sig1) + " - sig3 = "
+                        + Hex.b2s(sig3));
+        assertEquals("Signatures are not the same.", true, Hex.b2s(sig1).equals(Hex.b2s(sig3)));
 
         byte[] data1 = new byte[256]; // SHA256 hash is 32 bytes
         CE.SignInit(session, new CKM(CKM.ECDSA), privKey.value());
@@ -150,6 +130,82 @@ public class CryptokiTest extends TestCase {
             assertEquals("Failure with invalid signature data should be CKR.SIGNATURE_INVALID", CKR.SIGNATURE_INVALID,
                     e.getCKR());
         }
+    }
+
+    public void testGetSlotInfo() {
+        long session = CE.OpenSession(TESTSLOT, CK_SESSION_INFO.CKF_RW_SESSION | CK_SESSION_INFO.CKF_SERIAL_SESSION,
+                null, null);
+        CE.LoginUser(session, USER_PIN);
+
+        LongRef pubKey = new LongRef();
+        LongRef privKey = new LongRef();
+        generateKeyPairEd25519(session, pubKey, privKey);
+
+        CK_SLOT_INFO info = new CK_SLOT_INFO();
+        CE.GetSlotInfo(TESTSLOT, info);
+        System.out.println("testGetSlotInfo - Testslot: " + TESTSLOT);
+        System.out.println(info);
+
+        CK_TOKEN_INFO tinfo = new CK_TOKEN_INFO();
+        CE.GetTokenInfo(TESTSLOT, tinfo);
+        System.out.println("testGetSlotInfo - Token info: ");
+        System.out.println(tinfo);
+
+        final byte[] publicKey = CE.GetAttributeValue(session, pubKey.value(), CKA.VALUE).getValue();
+        System.out.println("testGetSlotInfo: public key size (should be 32 bytes) = " + publicKey.length + " - value = "
+                + Hex.b2s(publicKey));
+
+        try {
+            final byte[] privateKey = CE.GetAttributeValue(session, privKey.value(), CKA.VALUE).getValue();
+            fail("testGetSlotInfo: Obtaining private key value should throw exception");
+        } catch (CKRException e) {
+            assertEquals("testGetSlotInfo: Failure obtaining private key, should be CKR.ATTRIBUTE_SENSITIVE.",
+                    CKR.ATTRIBUTE_SENSITIVE,
+                    e.getCKR());
+            System.out.println("testGetSlotInfo: Failure obtaining private key, as expected:  " + CKR.L2S(e.getCKR()));
+        }
+    }
+
+    /**
+     * Generates a public-key / private-key Ed25519 pair, create new key objects.
+     * 
+     * @param session    the session's handle
+     * @param publicKey  gets handle of new public key
+     * @param privateKey gets handle of new private key
+     */
+    public void generateKeyPairEd25519(long session, LongRef publicKey, LongRef privateKey) {
+        // Attributes from PKCS #11 Cryptographic Token Interface Current Mechanisms
+        // Specification Version 2.40 section 2.3.3 - ECDSA public key objects
+        /*
+         * DER-encoding of an ANSI X9.62 Parameters, also known as
+         * "EC domain parameters".
+         */
+        // We use a Ed25519 key, the oid 1.3.101.112 has DER encoding in Hex 06032b6570
+        // In Utimaco, EC_PARAMS needs to have the value "edwards25519"
+
+        CKA[] pubTempl = new CKA[] {
+                new CKA(CKA.EC_PARAMS, "edwards25519"),
+                new CKA(CKA.WRAP, false),
+                new CKA(CKA.ENCRYPT, false),
+                new CKA(CKA.VERIFY, true),
+                new CKA(CKA.VERIFY_RECOVER, false),
+                new CKA(CKA.TOKEN, true),
+                new CKA(CKA.LABEL, "edwards-public"),
+                new CKA(CKA.ID, "labelec"),
+        };
+        CKA[] privTempl = new CKA[] {
+                new CKA(CKA.TOKEN, true),
+                new CKA(CKA.PRIVATE, true),
+                new CKA(CKA.SENSITIVE, true),
+                new CKA(CKA.SIGN, true),
+                new CKA(CKA.SIGN_RECOVER, false),
+                new CKA(CKA.DECRYPT, false),
+                new CKA(CKA.UNWRAP, false),
+                new CKA(CKA.EXTRACTABLE, false),
+                new CKA(CKA.LABEL, "edwards-private"),
+                new CKA(CKA.ID, "labelec"),
+        };
+        CE.GenerateKeyPair(session, new CKM(CKM.ECDSA_KEY_PAIR_GEN), pubTempl, privTempl, publicKey, privateKey);
     }
 
 }
